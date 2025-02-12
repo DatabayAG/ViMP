395f43524a1172403d32b21252e57d25f14d7558
Guido Vollbach <gvollbach@databay.de>
2025-02-12 12:06:06 +0100

    Fxied javascript problem

--- templates/js/xvmp_content.js
+++ templates/js/xvmp_content.js
@@ -108,9 +108,7 @@ var VimpContent = {
   copyDirectLinkWithTime: function (link_tpl) {
     let player_instance = videojs.getPlayers();
     let currentTime = '_' + Math.floor(Object.values(player_instance)[0].currentTime());
-    let link = link_tpl.replace('_0', currentTime).replace('_0&', currentTime + '&');
-    console.log(link)
-    console.log(link_tpl)
+    let link = link_tpl.replace('_0', '_' + currentTime).replace('_0&', currentTime + '&');
     this.copyToClipboard(link);
   },
 
