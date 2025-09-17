af14e5ddc97ac2fb123400f9e7318373f85b954e
Guido Vollbach <gvollbach@databay.de>
2025-09-17 08:27:48 +0200

    Add lp update

--- classes/GUI/class.xvmpLearningProgressGUI.php
+++ classes/GUI/class.xvmpLearningProgressGUI.php
@@ -35,6 +35,7 @@ class xvmpLearningProgressGUI extends xvmpGUI
             $selected_medium->update();
         }
         xvmpUserLPStatus::updateLPStatuses($this->getObjId(), false);
+        ilLPStatusWrapper::_refreshStatus($this->getObjId());
         $this->dic->ui()->mainTemplate()->setOnScreenMessage('success', $this->pl->txt('form_saved'), true);
         $this->dic->ctrl()->redirect($this, self::CMD_STANDARD);
     }
