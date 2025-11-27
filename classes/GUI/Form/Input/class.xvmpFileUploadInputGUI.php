65470c872a53f65fe544b1ee9fd3046081a67e6c
Guido Vollbach <gvollbach@databay.de>
2025-11-27 11:53:16 +0100

    urlencode filename before upload

--- classes/GUI/Form/Input/class.xvmpFileUploadInputGUI.php
+++ classes/GUI/Form/Input/class.xvmpFileUploadInputGUI.php
@@ -358,7 +358,7 @@ class xoctPlupload
             $fileName = uniqid("file_");
         }
 
-        $filePath = $targetDir . DIRECTORY_SEPARATOR . urlencode($fileName);
+        $filePath = $targetDir . DIRECTORY_SEPARATOR . rawurlencode($fileName);
         global $DIC;
         $ilLog = $DIC['ilLog'];
         $ilLog->write('plupload chunks');
